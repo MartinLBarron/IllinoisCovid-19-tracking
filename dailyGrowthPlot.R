ae28cfa2b1fcf9f76ebfba72207535b14e711e66
Martin Barron <martin@barron.cc>
2020-03-16 18:01:08 -0500

    added 3/16 data.  Added a growth rate chart and changed previously created chart to "% change" which is more accurate.

--- dailyGrowthPlot.R
+++ dailyGrowthPlot.R
@@ -13,7 +13,8 @@ library(scales)
 df <- read_csv("data/IL-covid-19-cases.csv") %>%
   mutate(Date=as.Date(Date, "%m/%d/%y")) %>%
   mutate(percentChange=(Illinois-lag(Illinois))/lag(Illinois),
-         newcases=Illinois-lag(Illinois))
+         newcases=Illinois-lag(Illinois),
+         growthFactor=newcases/lag(newcases))
 
 
 dfMostRecent <- df %>%
@@ -37,7 +38,7 @@ ggplot(data=df) +
   theme_minimal()+
   scale_y_continuous(labels = scales::percent)+
   scale_x_date(date_breaks = "2 day", date_labels = "%b %d")+
-  ggtitle("Daily Growth Rate in Illinois Coronavirus Cases") +
+  ggtitle("Daily % change in Illinois Coronavirus Cases") +
   annotate("text", x=as.Date("2020-03-06"), y=meangrowth+.03 ,label=meangrowthlabel)+
   xlab("") + 
   ylab("% Growth")+
@@ -50,6 +51,27 @@ ggplot(data=df) +
         axis.ticks.x=element_line(),
         axis.ticks.length.x = unit(3, "pt"))
 
-fname <- paste0("growth-", todayDate, ".png")
+fname <- paste0("percent_change-", todayDate, ".png")
 
 ggsave(here("output", fname), w=8, h=6)
+
+ggplot(data=df) +
+  geom_col(aes(x=Date, y=growthFactor), fill="#155F83") +
+  theme_minimal()+
+  scale_x_date(date_breaks = "2 day", date_labels = "%b %d")+
+  ggtitle("Daily Growth Rate in Illinois Coronavirus Cases") +
+  xlab("") + 
+  ylab("% Growth")+
+  labs(caption = "data from https://bit.ly/2IDLMlc")+
+  theme(panel.grid.minor.x=element_blank(),
+        panel.grid.major.x=element_blank(),
+        plot.title = element_text(hjust = .5),
+        panel.grid.minor.y=element_blank(),
+        axis.line.x = element_line(color="black", size = .5),
+        axis.ticks.x=element_line(),
+        axis.ticks.length.x = unit(3, "pt"))
+
+fname <- paste0("growth_rate-", todayDate, ".png")
+
+ggsave(here("output", fname), w=8, h=6)
+
